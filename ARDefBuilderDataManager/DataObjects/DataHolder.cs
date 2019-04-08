fda2cfb422760a7940888cd434eaa36a1db1bfd1
GraMMa <marcel@gramma.name>
2019-04-08 10:44:03 +0200

    Only fix the nulls, the other thing doesn't work properly

--- ARDefBuilderDataManager/DataObjects/DataHolder.cs
+++ ARDefBuilderDataManager/DataObjects/DataHolder.cs
@@ -6,22 +6,22 @@ namespace ARDefBuilderDataManager.DataObjects
 {
     public class DataHolder
     {
-        public List<HeroData> Heroes { get; set; }
+        public List<HeroData> Heroes { get; set; } = new List<HeroData>();
 
-        public List<SkillData> Weapons { get; set; }
+        public List<SkillData> Weapons { get; set; } = new List<SkillData>();
 
-        public List<SkillData> Assists { get; set; }
+        public List<SkillData> Assists { get; set; } = new List<SkillData>();
 
-        public List<SkillData> Specials { get; set; }
+        public List<SkillData> Specials { get; set; } = new List<SkillData>();
 
-        public List<SkillData> ASkills { get; set; }
+        public List<SkillData> ASkills { get; set; } = new List<SkillData>();
 
-        public List<SkillData> BSkills { get; set; }
+        public List<SkillData> BSkills { get; set; } = new List<SkillData>();
 
-        public List<SkillData> CSkills { get; set; }
+        public List<SkillData> CSkills { get; set; } = new List<SkillData>();
 
-        public List<SkillSealData> Seals { get; set; }
+        public List<SkillSealData> Seals { get; set; } = new List<SkillSealData>();
 
-        public StructureData Structures { get; set; }
+        public StructureData Structures { get; set; } = new StructureData();
     }
 }
